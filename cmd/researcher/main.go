433a2b31bd9c70f5b088c81378ce9ac6bdcbe672
DEHbNO4b <dehbno4b@gmail.com>
2024-01-15 09:23:02 +0000

    added meteo files search function

--- cmd/researcher/main.go
+++ cmd/researcher/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"meteo-lightning/internal/source/meteo"
+)
+
 type MeteoProvider interface {
 }
 
@@ -7,5 +12,27 @@ type LightningProvider interface {
 }
 
 func main() {
+	err := run()
+	if err != nil {
+		panic(err)
+	}
+}
 
+func run() error {
+
+	// Create meteo file sorce struct
+	s, err := meteo.New("./public/meteo", "public/meteo/*.txt")
+	if err != nil {
+		return err
+	}
+
+	// search files with meteo data
+	files, err := s.Search()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(files)
+
+	return nil
 }
--- internal/source/meteo/meteo.go
+++ internal/source/meteo/meteo.go
@@ -1,17 +1,48 @@
 package meteo
 
-import "meteo-lightning/internal/source"
+import (
+	"io/fs"
+	"meteo-lightning/internal/source"
+	"path/filepath"
+)
 
+// Metea data source
 type MeteoSource struct {
-	path string
+	path     string
+	template string
 }
 
-func New(p string) (MeteoSource, error) {
+func New(p, t string) (MeteoSource, error) {
 
-	if p == "" {
+	if p == "" || t == "" {
 		return MeteoSource{}, source.EmptyDataSource
 	}
 
-	return MeteoSource{path: p}, nil
+	return MeteoSource{path: filepath.FromSlash(p), template: filepath.FromSlash(t)}, nil
 
+}
+
+// Search new files with meteo data in path
+func (m *MeteoSource) Search() ([]string, error) {
+
+	var names []string
+
+	err := filepath.WalkDir(m.path, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if !d.IsDir() {
+			matched, err := filepath.Match(m.template, filepath.FromSlash(path))
+			if err != nil {
+				return err
+			}
+			if matched {
+				//читаем файлы совпавшие с заданной строкой в требуемой директории
+				names = append(names, path)
+			}
+		}
+		return nil
+	})
+
+	return names, err
 }
